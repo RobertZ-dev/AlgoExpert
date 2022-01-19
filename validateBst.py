79e04f08e96ae9e3077627f813d090f6fcf43869
januswizard <januzwizard@gmail.com>
2022-01-18 22:12:10 -0800

    Update validateBst.py

--- validateBst.py
+++ validateBst.py
@@ -16,4 +16,5 @@ def validateBstHelper(tree, minValue, maxValue):
 	if tree.value < minValue or tree.value >= maxValue:
 		return False
 	leftIsValid = validateBstHelper(tree.left, minValue, tree.value)
-	return leftIsValid and validateBstHelper(tree.right, tree.value, maxValue)
+	rightIsValid = validateBstHelper(tree.right, tree.value, maxValue)
+	return leftIsValid and rightIsValid
