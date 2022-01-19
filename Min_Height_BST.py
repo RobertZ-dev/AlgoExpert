ace8a15df51abf94dc92bfb943cd6a650f4f37e1
januswizard <januzwizard@gmail.com>
2022-01-19 00:08:04 -0800

    Create Min_Height_BST.py

---
+++ Min_Height_BST.py
@@ -0,0 +1,31 @@
+# O(n) time | O(n) space
+def minHeightBst(array):
+	return constructMinHeightBst(array, None, 0, len(array) - 1)
+
+def constructMinHeightBst(array, bst, startIdx, endIdx):
+	if endIdx < startIdx:
+		return None
+	midIdx = (startIdx + endIdx)//2
+	bst = BST(array[midIdx])
+	bst.left = constructMinHeightBst(array, bst, startIdx, midIdx - 1)
+	bst.right = constructMinHeightBst(array, bst, midIdx+1, endIdx)
+    return bst
+
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value)
+            else:
+                self.right.insert(value)
