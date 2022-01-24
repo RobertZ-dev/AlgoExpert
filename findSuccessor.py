837a854ce85a878c7ec5d2c6bbe40f174736b102
januswizard <bobstar@gmail.com>
2022-01-23 21:31:38 -0800

    Create findSuccessor.py

---
+++ findSuccessor.py
@@ -0,0 +1,25 @@
+# This is an input class. Do not edit.
+class BinaryTree:
+    def __init__(self, value, left=None, right=None, parent=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+# O(h) time | O(1) space
+def findSuccessor(tree, node):
+    if node.right is not None:
+		return getLeftmostChild(node.right)
+	return getRightmostParent(node)
+
+def getLeftmostChild(node):
+	currentNode = node
+	while currentNode.left is not None: 
+		currentNode = currentNode.left
+	return currentNode
+
+def getRightmostParent(node):
+	currentNode = node
+	while currentNode.parent is not None and currentNode.parent.right == currentNode:
+		currentNode = currentNode.parent
+	return currentNode.parent
