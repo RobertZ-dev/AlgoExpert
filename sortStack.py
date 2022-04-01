52cc861ccf1befe6d9e48469e12bf401d2abd349
januswizard <bobstar@gmail.com>
2022-03-31 18:01:09 -0700

    Create sortStack.py

---
+++ sortStack.py
@@ -0,0 +1,16 @@
+# O(n^2) time | O(n) space
+def sortStack(stack):
+	if len(stack) == 0:
+		return stack
+	top = stack.pop()
+	sortStack(stack)
+	insert(stack, top)
+	return stack
+
+def insert(stack, value):
+	if len(stack) == 0 or stack[len(stack) - 1] <= value:
+		stack.append(value)
+		return
+	top = stack.pop()
+	insert(stack,value)
+	stack.append(top)
