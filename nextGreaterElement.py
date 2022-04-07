bb25a6f05020ba94b813d279d36bada077ebf759
januswizard <bobstar@gmail.com>
2022-04-07 16:59:12 -0700

    Update nextGreaterElement.py

--- nextGreaterElement.py
+++ nextGreaterElement.py
@@ -1,10 +1,14 @@
+# O(n) time | O(n) space
 def nextGreaterElement(array):
 	result = [-1] * len(array)
 	stack = []
-	for idx in range(2 * len(array)):
+	for idx in range(2 * len(array) - 1, -1, -1):
 		circularIdx = idx % len(array)
-		while len(stack) > 0 and array[stack[len(stack) - 1]] < array[circularIdx]:
-			top = stack.pop()
-			result[top] = array[circularIdx]
-		stack.append(circularIdx)
+		while len(stack) > 0:
+			if stack[len(stack) - 1] <= array[circularIdx]:
+				stack.pop()
+			else:
+				result[circularIdx] = stack[len(stack) - 1]
+				break
+		stack.append(array[circularIdx])
     return result
